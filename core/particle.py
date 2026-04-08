76f69935f0df69e339eb14485eeeb9e587d2ff8f
David Ortega <david.ortega@cunef.edu>
2026-04-08 09:10:10 +0200

    Update of the core directory to include a configuration file, addition of more descriptive comments to explain how the methods work, and refactoring of the code to make it easier to read.

--- core/particle.py
+++ core/particle.py
@@ -2,7 +2,7 @@
 # Class representing a particle in the PSO algorithm
 # @author: David Ortega Lozano
 # @date: 2026-02-25
-# @version: 0.2
+# @version: 0.4
 # @description: Each particle has a position, velocity, personal best position, and
 # personal best value.
 #  The particle can update its velocity based on the global best position and its
@@ -10,18 +10,33 @@
 #----------------------------------------------------------------------------------
 
 import numpy as np
+import json
 
-MAX = 100
-MIN = -100
+with open('config.json') as f:
+    config = json.load(f)
 
 class Particle:
-    def __init__(self, position, velocity):
+    def __init__(self, 
+                 position, 
+                 velocity
+                 ) -> None:
+        
         self.position = position
         self.velocity = velocity
-        self.best_position = position
+        self.best_position = position.copy()
         self.best_value = float('inf')
+        self.logs = []
     
-    def update_velocity(self, global_best_position, inertia_weight=0.5, cognitive_weight=1.0, social_weight=1.0):
+    # The update_velocity method updates the velocity of the particle based on the 
+    # global best position, its own best position, and the inertia weight, 
+    # cognitive weight, and social weight parameters.
+    def update_velocity(self, 
+                        global_best_position, 
+                        inertia_weight=0.5, 
+                        cognitive_weight=0.5, 
+                        social_weight=0.5
+                        ) -> None:
+        
         r1 = np.random.random()
         r2 = np.random.random()
         for i in range(len(self.position)):
@@ -29,15 +44,34 @@ class Particle:
             social_component = social_weight * r2 * (global_best_position[i] - self.position[i])
             self.velocity[i] = inertia_weight * self.velocity[i] + cognitive_component + social_component
 
-    def update_position(self):
+    # The update_position method updates the position of the particle based on its 
+    # velocity and ensures that it stays within the defined bounds.
+    def update_position(self) -> None:
+        
         self.position += self.velocity
 
-        mask_min = self.position < MIN
-        mask_max = self.position > MAX
+        mask_min = self.position < config["MIN"]
+        mask_max = self.position > config["MAX"]
 
         # Rebote de velocidad
-        self.velocity[mask_min] = - (self.position[mask_min] - MIN)
-        self.velocity[mask_max] = - (self.position[mask_max] - MAX)
+        self.velocity[mask_min] = - (self.position[mask_min] - config["MIN"])
+        self.velocity[mask_max] = - (self.position[mask_max] - config["MAX"])
 
         # Limitar posición
-        self.position = np.clip(self.position, MIN, MAX)
+        self.position = np.clip(self.position, 
+                                config["MIN"], 
+                                config["MAX"])
+
+    # The save_log method saves the current position, value, global best position,
+    # and global best value of the particle in its logs list. This allows for 
+    # tracking the history of the particle's performance over time.
+    def save_log(self, 
+                 value, 
+                 global_best_position, 
+                 global_best_value
+                 ) -> None:
+        
+        self.logs.append((self.position.copy(), 
+                          value, 
+                          global_best_position, 
+                          global_best_value))
--- core/swarm.py
+++ core/swarm.py
@@ -2,16 +2,10 @@
 # Class representing the PSO algorithm.
 # @author: David Ortega Lozano
 # @date: 2026-02-27
-# @version: 0.3
+# @version: 0.4
 # @description: It initializes a swarm of particles and iteratively updates their 
 # positions and velocities to find the optimal solution to the given objective 
 # function.
-#  The optimize method runs the main loop of the PSO algorithm, where it evaluates
-# the objective function for each particle, updates their personal bests and the
-# global best, and then updates their velocities and positions accordingly.
-#  The Swarm class takes the number of particles, the number of dimensions, and the
-# objective function as input parameters. The random_num method generate random 
-# initial positions and velocities for the particles within specified ranges.
 #----------------------------------------------------------------------------------
 
 from core.particle import Particle
@@ -21,30 +15,73 @@ from objectives.sphere import sphere_function
 from objectives.rosenbrock import rosenbrock_function
 from objectives.rastrigin import rastrigin_function
 from objectives.ackley import ackley_function
+from io_utiles.methods import save_logs
 import numpy as np
+import json
 
-V_MAX = 1000000
+with open('config.json') as f:
+    config = json.load(f)
 
 class Swarm:
-    def __init__(self, num_particles, objective_function, num_dimensions = 2):
+    # The Swarm class takes the number of particles, the number of dimensions, and 
+    # the objective function as input parameters.
+    def __init__(self, 
+                 num_particles, 
+                 objective_function, 
+                 num_dimensions = 2
+                 ) -> None:
+        
         self.num_particles = num_particles
         self.num_dimensions = num_dimensions
         self.objective_function = objective_function
-        self.particles = [Particle(self.random_num(-10, 10), self.random_num(-1, 1)) for _ in range(num_particles)]
+        self.particles = [Particle(self.random_num(-100, 100), 
+                                   self.random_num(-1, 1)) 
+                                   for _ in range(num_particles)]
         self.global_best_position, self.global_best_value = self.get_first_global_best()
     
-    def random_num(self, min, max):
-        return np.array([np.random.uniform(min, max, self.num_dimensions)])
+    # The random_num method generate random initial positions and velocities for 
+    # the particles within specified ranges.
+    def random_num(self, 
+                   min, 
+                   max
+                   ) -> np.ndarray:
+        
+        return np.random.uniform(min, max, self.num_dimensions)
     
-    def optimize(self, max_iterations, method = 1):
-        for iteration in range(max_iterations):
+    # The optimize method runs the main loop of the PSO algorithm, where it 
+    # evaluates the objective function for each particle, updates their personal 
+    # bests and the global best, and then updates their velocities and positions 
+    # accordingly.
+    def optimize(self,
+                 method = 1
+                 ) -> None:
+        
+        objective_value = 1 if self.objective_function == 3 else 0
+
+        for iteration in range(config["ITERATIONS"]):
             match method:
                 case 1:
-                    self.global_best_position, self.global_best_value = secuential(self.particles, self.objective_function, self.global_best_position, self.global_best_value)
+                    self.global_best_position, self.global_best_value = secuential(self.particles, 
+                                                                                   self.objective_function, 
+                                                                                   self.global_best_position, 
+                                                                                   self.global_best_value)
                 case 2:
-                    self.global_best_position, self.global_best_value = threading_function(self.particles, self.objective_function, self.global_best_position, self.global_best_value)
-    
-    def get_first_global_best(self):
+                    self.global_best_position, self.global_best_value = threading_function(self.particles, 
+                                                                                           self.objective_function, 
+                                                                                           self.global_best_position, 
+                                                                                           self.global_best_value)
+            
+            if (self.global_best_value <= (config["ERROR"] + objective_value)
+                and 
+                self.global_best_value >= (-config["ERROR"]) + objective_value):
+                break
+
+        save_logs(self, method)
+
+    # The get_first_global_best method initializes the global best position and 
+    # value by evaluating the objective function for the initial positions of all 
+    # particles and selecting the best one as the initial global best.
+    def get_first_global_best(self) -> tuple:
         bestP = self.particles[0].position
         match self.objective_function:
             case 1:
@@ -70,4 +107,6 @@ class Swarm:
             if value < bestV:
                 bestP = particle.position
                 bestV = value
+
         return bestP, bestV
+    
